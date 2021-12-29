03707200247c1c262c5e914561576bc724790f8d
cc97nitu <ccaliari@ikp.tu-darmstadt.de>
2021-12-29 16:06:14 +0100

    added per-parameter masks / training example for SIS18

--- src/Beamlines.jl
+++ src/Beamlines.jl
@@ -20,3 +20,48 @@ function NestedChain(cells, args...; kwargs...)
     
     return Flux.Chain(beamline..., args...; kwargs...)
 end
+
+"""
+    track(model::Flux.Chain, batch::DenseArray)::PermutedDimsArray
+
+Track particles through model and collect phasespace after each layer.
+"""
+function track(model::Flux.Chain, batch::DenseArray)::PermutedDimsArray
+    out = reduce(hcat, Flux.activations(model, batch))
+    out = reshape(out, 7, :, length(model))  # dim, particle, BPM
+    return PermutedDimsArray(out, (1,3,2))  # dim, BPM, particle
+end
+
+"""
+    assignMasks(model; nested=true, quadMask=nothing, sextMask=nothing)::IdDict{Vector{Float64}, Vector{Float64}}
+
+Return dict which maps kn to their corresponding mask.
+"""
+function assignMasks(model::Flux.Chain;
+    nested::Bool=true, quadMask::Union{Vector{Float64}, Nothing}=nothing, sextMask::Union{Vector{Float64}, Nothing}=nothing
+    )::IdDict{Vector{Float64}, Vector{Float64}}
+
+    masks = IdDict{Vector{Float64}, Vector{Float64}}()
+
+    if nested
+        for cell in model
+            for element in cell
+                if typeof(element) == Quadrupole && !isnothing(quadMask) 
+                    masks[element.kn] = quadMask
+                elseif typeof(element) == Sextupole && !isnothing(sextMask)
+                    masks[element.kn] = sextMask
+                end
+            end
+        end
+    else
+        for element in model
+            if typeof(element) == Quadrupole && !isnothing(quadMask) 
+                masks[element.kn] = quadMask
+            elseif typeof(element) == Sextupole && !isnothing(sextMask)
+                masks[element.kn] = sextMask
+            end
+        end
+    end
+
+    return masks
+end
--- src/Lattice.jl
+++ src/Lattice.jl
@@ -48,4 +48,54 @@ function SIS18_Lattice_minimal(k1f::Float64, k1d::Float64; nested::Bool=true, ce
 end
 
 
+function SIS18_Cell(k1f::Float64, k1d::Float64, k2f::Float64, k2d::Float64)
+    # bending magnets
+    bendingAngle = 0.2617993878
+    rb1 = ThinLens.RBen(2.617993878, bendingAngle, 0, 0)
+    rb2 = ThinLens.RBen(2.617993878, bendingAngle, 0, 0)
+
+    # quadrupoles
+    qs1f = ThinLens.Quadrupole(1.04, k1f, 0)
+    qs2d = ThinLens.Quadrupole(1.04, k1d, 0)
+    qs3t = ThinLens.Drift(0.4804)
+
+    # sextupoles
+    ks1c = ThinLens.Sextupole(0.32, k2f, 0)
+    ks3c = ThinLens.Sextupole(0.32, k2d, 0)
+   
+    # drifts
+    d1 = ThinLens.Drift(0.66355)
+    d2 = ThinLens.Drift(0.9700000000000002)
+    d3a = ThinLens.Drift(6.345)
+    d3b = ThinLens.Drift(0.175)
+    d4 = ThinLens.Drift(0.5999999999999979)
+    d5a = ThinLens.Drift(0.195)
+    d5b = ThinLens.Drift(0.195)
+
+    hMon = ThinLens.Drift(0.48125)
+
+    # set up beamline
+    #Flux.Chain(d1, rb1, d2, rb2 d3a, ks1c, d3b, qs1f, d4, qs2d, d5a, ks3c, d5b, qs3t, d6a, hMon, vMonDrift, d6b)  # actual layout
+    Flux.Chain(d1, rb1, d2, rb2, d3a, ks1c, d3b, qs1f, d4, qs2d, d5a, ks3c, d5b, qs3t, hMon)
+end
+
+
+function SIS18_Lattice(k1f::Float64, k1d::Float64, k2f::Float64, k2d::Float64; nested::Bool=true, cellsIdentical::Bool=false)
+    if nested
+        if cellsIdentical
+            cell = SIS18_Cell(k1f, k1d, k2f, k2d)
+            return ThinLens.NestedChain([cell for _ in 1:12])
+        else
+            return ThinLens.NestedChain([SIS18_Cell(k1f, k1d, k2f, k2d) for _ in 1:12])
+        end
+    else
+        if cellsIdentical
+            cell = SIS18_Cell(k1f, k1d, k2f, k2d)
+            return ThinLens.FlatChain([cell for _ in 1:12])
+        else
+            return ThinLens.FlatChain([SIS18_Cell(k1f, k1d, k2f, k2d) for _ in 1:12])
+        end
+    end
+end
+
 end  # module
