9ca49a6ee10e2f739214edb2f60bc94e106e00aa
cc97nitu <ccaliari@ikp.tu-darmstadt.de>
2022-10-10 09:12:57 +0200

    added SnoopPrecompile - no big change

--- src/ThinLens.jl
+++ src/ThinLens.jl
@@ -1,6 +1,9 @@
 module ThinLens
 
 import ChainRulesCore
+import Flux
+using SnoopPrecompile
+
 
 greet() = print("Hello World!")
 
@@ -17,13 +20,24 @@ include("Beamlines.jl")
 include("Lattice.jl")
 include("Tools.jl")
 
-# Flux.@functor Drift
-Flux.@functor Quadrupole
-Flux.@functor Sextupole
-# Flux.@functor BendingMagnet
-# Flux.@functor RBen
-# Flux.@functor SBen
+@precompile_setup begin
+    # Putting some things in `setup` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    particle = [-8.16e-03, -1.78e-03,  5.55e-03,  1.93e-03,  0.00e+00,  7.87e-04, 9.99e-01];
+    model = Lattice.SIS18_Lattice(0.3509739273586273, -0.3090477494318206, 0., 0.; split=splitO2nd, steps=4);
+    accelerator = Lattice.SIS18_Lattice(0.3509739273586273, -0.3090477494318206, 0.01, 0.; split=splitO2nd, steps=4);
+    label = accelerator(particle)
 
+    @precompile_all_calls begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
+        sample = model(particle)
+        parameters = Flux.params(model)
+        grads = Flux.gradient(parameters) do
+            Flux.Losses.mse(sample, label)
+        end
+    end
+end
 
 
 end # module
